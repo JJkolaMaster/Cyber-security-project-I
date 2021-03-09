764d1728119b75f3fbead8e83133f2115523ee11
Joni Jaakkola <j0n1.j44kk0l4@hotmail.com>
2021-03-09 15:01:29 +0200

    new start

---
+++ security_project_I/cyber_blog/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class CyberBlogConfig(AppConfig):
+    name = 'cyber_blog'
---
+++ security_project_I/cyber_blog/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+
+posts =[
+    {
+        'author': 'Iiris Kissanen',
+        'title': 'Cyber Blog Post 1',
+        'content': 'First cat post',
+        'date_posted': '27 July 2020'
+    },
+    {
+        'author': 'Mosse Kissanen',
+        'title': 'Cyber Blog Post 2',
+        'content': 'Second cat post',
+        'date_posted': '28 July 2020'  
+    }
+]
+
+def home (request):
+    context = {
+        'posts': posts
+    }
+    return render(request, 'cyber_blog/home.html', context)
+
+def about (request):
+    return render(request, 'cyber_blog/about.html')
+
